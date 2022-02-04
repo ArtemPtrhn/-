5473b5bc81216c56e7dd428cadac05236230267c
ArtemPtrhn <98023058+ArtemPtrhn@users.noreply.github.com>
2022-02-04 15:22:29 +0300

    Add files via upload

--- serverrr.py
+++ serverrr.py
@@ -5,11 +5,15 @@ get_flashed_messages() - в шаблоне
 flash() -> session -> get_flashed_messages() -> шаблон
 '''
 
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, abort, session
 
 
 app = Flask(__name__)
 app.config['SECRET.KEY'] = 'dfkjhbvrituniuibufd'
+users = [
+    {'username': 'artem', 'password': '12345', 'is_admin': True},
+    {'username': 'student', 'password': '1060', 'is_admin': False},
+]
 
 
 @app.route('/')
@@ -55,10 +59,37 @@ def form():
         if len(request.form['fullname']) < 5 and not request.form['fullname'].isalpha():
             flash('Ошибка в имени. Сообщение не отправлено!', category='error')
         else:
-            flash('Сообщение принято!', category='success')      
+            flash('Сообщение принято!', category='success')
         for item in request.form:
             print(item, request.form[item])
     return render_template('form.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        for user in users:
+            if request.form['username'] == user['username']:
+                if request.form['password'] == user['password']:
+                    session['logged_in'] = True
+                    return redirect(f"/profile/{user['username']}")
+                else:
+                    flash('Неправильный пароль', category='error')
+                    break
+            else:
+                flash('Неправильный логин', category='error')
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    for user in users:
+        if user['username'] == username:
+            if 'logged_in' in session:
+                if session['logged_in'] == username:
+                    return render_template('profile.html', username=username)
+            flash('Вам туда нельзя. Залогинтесь', category='error')
+            return redirect(url_for('login'))
+    abort(404)
 
 
 if __name__ == '__main__':
